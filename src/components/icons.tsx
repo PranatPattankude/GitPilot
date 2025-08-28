dd1483d15e5d4d2856b62ca4255ee9850751648a
Panu <panu4358@gmail.com>
2025-08-28 11:05:09 +0000

    meargeing logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,18 +12,10 @@ export function GitPilotLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 2v4" />
-      <path d="M12 18v4" />
-      <path d="M22 12h-4" />
-      <path d="M6 12H2" />
-      <path d="m19.07 4.93-3.12 3.12" />
-      <path d="m8.05 15.95-3.12 3.12" />
-      <path d="m19.07 19.07-3.12-3.12" />
-      <path d="m8.05 8.05-3.12-3.12" />
-      <circle cx="12" cy="12" r="2" />
-      <path d="M15.2 15.2 18 18" />
-      <path d="m12 8-2.5 2.5" />
-      <path d="m8 12 2.5 2.5" />
+      <circle cx="18" cy="18" r="3" />
+      <circle cx="6" cy="6" r="3" />
+      <path d="M6 9v6a3 3 0 0 0 3 3h3" />
+      <path d="M18 9v-3a3 3 0 0 0-3-3H9" />
     </svg>
   );
 }
