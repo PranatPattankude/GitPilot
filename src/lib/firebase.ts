d112800770671b0c1c12ea986eed6c8af3223a01
Panu <panu4358@gmail.com>
2025-08-28 09:01:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,10 +4,9 @@ import { getAuth, GithubAuthProvider } from 'firebase/auth';
 const firebaseConfig = {
   projectId: 'gitpilot-8qubd',
   appId: '1:881264771425:web:d2abc7497c39f380ef8520',
-  storageBucket: 'gitpilot-8qubd.firebasestorage.app',
+  storageBucket: 'gitpilot-8qubd.appspot.com',
   apiKey: 'AIzaSyAfCLUkK1Bsz9PcRWrCZO6JjBpKuWIhRJs',
   authDomain: 'gitpilot-8qubd.firebaseapp.com',
-  measurementId: '',
   messagingSenderId: '881264771425',
 };
 
