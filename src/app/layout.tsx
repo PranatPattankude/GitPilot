d268555bb44e2ee62712bdb4e737ec5b0ee0802c
Panu <panu4358@gmail.com>
2025-08-29 12:05:52 +0000

    Runtime Error
    Error: Failed to fetch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased">
-        <SessionProvider session={session}>
+        <SessionProvider session={session} basePath="/api/auth">
           {children}
         </SessionProvider>
         <Toaster />
