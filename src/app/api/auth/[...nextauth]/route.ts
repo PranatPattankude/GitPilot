9cc5420991aa996fb4b8fb35cd966d8e87fd83ec
Panu <panu4358@gmail.com>
2025-08-29 15:35:10 +0000

    https://next-auth.js.org/errors#oauth_callback_error State cookie was mi

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,23 +7,35 @@ const handler = NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+      ...(process.env.NODE_ENV === 'development' && {
+        authorization: {
+          params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code'
+          }
+        }
+      })
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token
       }
       return token
     },
     async session({ session, token }) {
-      // You can pass custom properties to the session object here
-      // For example, if you need the access token on the client side:
-      // (session.accessToken as any) = token.accessToken;
+      // Send properties to the client, like an access_token from a provider.
+      (session as any).accessToken = token.accessToken
       return session
     },
   },
+  ...(process.env.NODE_ENV === 'development' && {
+    checks: ['state']
+  }),
 })
 
 export { handler as GET, handler as POST }
